b992a4337f1497e29855d192de86a0861ae599d6
Marc Muntané <mmclara1050@gmail.com>
2024-09-19 16:43:24 +0200

    Issue-Marc

--- Models/Player.php
+++ Models/Player.php
@@ -2,16 +2,13 @@
 
 namespace App\Models;
 
-
 /**
  * Represents play field.
  */
 class Player extends Person
 {
     /**
-
      * Player name
-
      *
      * @var string
      */
@@ -29,15 +26,13 @@ class Player extends Person
      *
      * @param string $name     Player name
      * @param bool $active  Player Status
-     * @param bool $motivation  Player Motivation
-
-
      */
     public function __construct(string $name, bool $active)
     {
         $this->name = $name;
         $this->active = $active;
     }
+
     /**
      * Get Player name
      *
@@ -47,6 +42,7 @@ class Player extends Person
     {
         return $this->name;
     }
+
     /**
      * Get Player Status
      *
@@ -58,14 +54,6 @@ class Player extends Person
     }
 
     /**
-     * Get Player Motivation
-     *
-     * @return  bool
-     */
-
-
-
-    /**
      * Set Player name in string
      *
      * @param  string  $name is string
@@ -75,9 +63,9 @@ class Player extends Person
     public function setName(string $name)
     {
         $this->name = $name;
-
         return $this;
     }
+
     /**
      * Set status in bool
      *
@@ -88,8 +76,46 @@ class Player extends Person
     public function setActive(bool $active)
     {
         $this->active = $active;
-
         return $this;
     }
 
+    // Parte Marc
+    /**
+     * Pass the ball to another player.
+     *
+     * @param Player $player The player to whom the ball is passed
+     *
+     * @return void
+     */
+    public function passBall(Player $player)
+    {
+        // Create a Ball object
+        $ball = new class {
+            /**
+             * The material of the ball.
+             *
+             * @var string
+             */
+            private $material = 'leather';
+
+            /**
+             * Kick the ball.
+             *
+             * @return void
+             */
+            public function kick()
+            {
+                echo "The ball made of " . $this->material . " is kicked and spins!";
+            }
+        };
+
+        // Check if the player is active
+        if ($this->active) {
+            echo $this->getName() . " passes the ball to " . $player->getName() . ". ";
+            // Simulate ball kick and spin
+            $ball->kick();
+        } else {
+            echo $this->getName() . " is not active and cannot pass the ball.";
+        }
+    }
 }
